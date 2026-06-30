bc52acbab559528e9a8ede608e9c8ae04f8dce56
Sohail Shehram <sohailgill4949@gmail.com>
2026-06-30 16:47:05 +0500

    Fix templates directory

--- config/settings.py
+++ config/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,7 +75,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
